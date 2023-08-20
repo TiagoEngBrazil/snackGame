39b538ee614b3d34b169244985eeb14f47df7f49
B owie <tiago.pimenta@ucsal.edu.br>
2023-08-20 15:47:50 -0300

    "fix the button!!"

--- app/src/main/java/com/example/snackgame/MainActivity.kt
+++ app/src/main/java/com/example/snackgame/MainActivity.kt
@@ -28,6 +28,7 @@ import com.example.snackgame.ui.theme.Shapes
 import com.example.snackgame.ui.theme.SnackGameTheme
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
@@ -58,13 +59,13 @@ data class State(val food: Pair<Int, Int>, val snack: List<Pair<Int, Int>>)
 
 class Game(private val scope: CoroutineScope) {
 
-    private val mutex: Mutex = Mutex()
+    private val mutex = Mutex()
     private val mutableState =
         MutableStateFlow(State(food = Pair(5, 5), snack = listOf(Pair(7, 7))))
-    val state = mutableState
+    val state: Flow<State> = mutableState
 
     var move: Pair<Int, Int> = Pair(1, 0)
-        set(value: Pair<Int, Int>) {
+        set(value) {
             scope.launch {
                 mutex.withLock {
                     field = value
@@ -79,11 +80,11 @@ class Game(private val scope: CoroutineScope) {
             while (true) {
                 delay(150)
                 mutableState.update {
-                    val newPosition: Pair<Int, Int> = it.snack.first().let { poz: Pair<Int, Int> ->
+                    val newPosition = it.snack.first().let { poz ->
                         mutex.withLock {
                             Pair(
-                                (poz.first + move.first) % BOARD_SIZE,
-                                (poz.second + move.second) % BOARD_SIZE
+                                (poz.first + move.first + BOARD_SIZE) % BOARD_SIZE,
+                                (poz.second + move.second + BOARD_SIZE) % BOARD_SIZE
                             )
                         }
                     }
